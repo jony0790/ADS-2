96c965a4ef30015518cb6a8f6ef96698ecd05ab5
Джалоладдин <joniatabekovich07@gmail.com>
2026-06-05 18:11:19 +0300

    Джумабаев Джалоладдин 25-ИСТ-1-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,8 @@
 // Copyright 2025
 
-#include <cstdint>
+#include "alg.h"
 
-double pow(double value, uint16_t n) {
+double pown(double value, uint16_t n) {
     double result = 1.0;
 
     for (uint16_t i = 0; i < n; ++i) {
@@ -12,8 +12,8 @@ double pow(double value, uint16_t n) {
     return result;
 }
 
-double fact(uint16_t n) {
-    double result = 1.0;
+uint64_t fact(uint16_t n) {
+    uint64_t result = 1;
 
     for (uint16_t i = 2; i <= n; ++i) {
         result *= i;
@@ -23,10 +23,10 @@ double fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-    return pow(x, n) / fact(n);
+    return pown(x, n) / static_cast<double>(fact(n));
 }
 
-double exp(double x, uint16_t count) {
+double expn(double x, uint16_t count) {
     double sum = 0.0;
 
     for (uint16_t n = 0; n < count; ++n) {
@@ -36,27 +36,27 @@ double exp(double x, uint16_t count) {
     return sum;
 }
 
-double sin(double x, uint16_t count) {
+double sinn(double x, uint16_t count) {
     double sum = 0.0;
 
     for (uint16_t n = 1; n <= count; ++n) {
         double sign = (n % 2 == 1) ? 1.0 : -1.0;
 
-        sum += sign * pow(x, 2 * n - 1) /
-               fact(2 * n - 1);
+        sum += sign * pown(x, 2 * n - 1) /
+               static_cast<double>(fact(2 * n - 1));
     }
 
     return sum;
 }
 
-double cos(double x, uint16_t count) {
+double cosn(double x, uint16_t count) {
     double sum = 0.0;
 
     for (uint16_t n = 1; n <= count; ++n) {
         double sign = (n % 2 == 1) ? 1.0 : -1.0;
 
-        sum += sign * pow(x, 2 * n - 2) /
-               fact(2 * n - 2);
+        sum += sign * pown(x, 2 * n - 2) /
+               static_cast<double>(fact(2 * n - 2));
     }
 
     return sum;
