93c8518a2e43ed65977f1d40a1b5660b15044afb
Джалоладдин <joniatabekovich07@gmail.com>
2026-06-05 17:58:04 +0300

    Джумабаев Джалоладдин 25-ИСТ-1-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,82 @@
-// Copyright 2022 NNTU-CS
 #include <cstdint>
-#include "alg.h"
 
+// value^n
+double pow(double value, uint16_t n)
+{
+    double result = 1.0;
 
-double pown(double value, uint16_t n) {
-  return 0.0;
+    for (uint16_t i = 0; i < n; i++)
+    {
+        result *= value;
+    }
+
+    return result;
 }
 
-uint64_t fact(uint16_t n) {
-  return 0.0;
+// n!
+uint64_t fact(uint16_t n)
+{
+    uint64_t result = 1;
+
+    for (uint16_t i = 2; i <= n; i++)
+    {
+        result *= i;
+    }
+
+    return result;
 }
 
-double calcItem(double x, uint16_t n) {
-  return 0.0;
+// x^n / n!
+double calcItem(double x, uint16_t n)
+{
+    return pow(x, n) / (double)fact(n);
 }
 
-double expn(double x, uint16_t count) {
-  return 0.0;
+// e^x
+double exp(double x, uint16_t count)
+{
+    double sum = 0.0;
+
+    for (uint16_t n = 0; n < count; n++)
+    {
+        sum += calcItem(x, n);
+    }
+
+    return sum;
 }
 
-double sinn(double x, uint16_t count) {
-  return 0.0;
+// sin(x)
+double sin(double x, uint16_t count)
+{
+    double sum = 0.0;
+
+    for (uint16_t n = 1; n <= count; n++)
+    {
+        double term =
+            pow(-1.0, n - 1) *
+            pow(x, 2 * n - 1) /
+            (double)fact(2 * n - 1);
+
+        sum += term;
+    }
+
+    return sum;
 }
 
-double cosn(double x, uint16_t count) {
-  return 0.0;
+// cos(x)
+double cos(double x, uint16_t count)
+{
+    double sum = 0.0;
+
+    for (uint16_t n = 1; n <= count; n++)
+    {
+        double term =
+            pow(-1.0, n - 1) *
+            pow(x, 2 * n - 2) /
+            (double)fact(2 * n - 2);
+
+        sum += term;
+    }
+
+    return sum;
 }
