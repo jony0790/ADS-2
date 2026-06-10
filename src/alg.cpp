941d3adc7143ea3cded65bd2fa5885446f700337
Джалоладдин <joniatabekovich07@gmail.com>
2026-06-10 04:24:20 +0300

    Джумабаев Джалоладдин 25-ИСТ-1-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,71 @@
-// Copyright 2022 NNTU-CS
-#include <cstdint>
+// Copyright 2025
+
 #include "alg.h"
-
-
+#include <cstdint>
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double result = 1.0;
+
+    for (uint16_t i = 0; i < n; ++i) {
+        result *= value;
+    }
+
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t result = 1;
+
+    for (uint16_t i = 2; i <= n; ++i) {
+        result *= i;
+    }
+
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pown(x, n) / static_cast<double>(fact(n));
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+
+    for (uint16_t n = 0; n <=count; ++n) {
+        result += calcItem(x, n);
+    }
+
+    return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+
+    for (uint16_t n = 0; n < count; ++n) {
+        double term = pown(x, 2 * n + 1) /
+            static_cast<double>(fact(2 * n + 1));
+
+        if (n % 2 == 1) {
+            term = -term;
+        }
+
+        result += term;
+    }
+
+    return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+
+    for (uint16_t n = 0; n < count; ++n) {
+        double term = pown(x, 2 * n) /
+            static_cast<double>(fact(2 * n));
+
+        if (n % 2 == 1) {
+            term = -term;
+        }
+
+        result += term;
+    }
+
+    return result;
 }
